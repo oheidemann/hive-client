acfb49d9d8d9366d32c0ad2b8ae82df896d9008a
Olivier Heidemann <olivier.heidemann@bcgdv.com>
2018-11-30 12:20:36 +0100

    nix

--- src/main/scala/domain/Main.scala
+++ src/main/scala/domain/Main.scala
@@ -1,5 +1,7 @@
 package domain
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
@@ -11,14 +13,37 @@ object Main extends App {
     val akkaConf = config.getConfig("adp-client")
     val system = ActorSystem("adpSystem", akkaConf)
 
-    system.actorOf(Props[StupidPrisonerActor])
+    system.actorOf(Props[RandomPrisonerActor])
   }
 }
 
-class StupidPrisonerActor extends Actor with ActorLogging {
-  override def receive: Receive = {
-    case unhandled =>
-      log.error(s"received message [${unhandled.getClass.getSimpleName}] but didn't handle it!")
+class RandomPrisonerActor extends Actor with ActorLogging {
+  import domain.PrisonerProtocol._
 
+  val name = UUID.randomUUID().toString
+
+  val rnd = new scala.util.Random(System.currentTimeMillis())
+
+  def receive = {
+    case Captured(prison) =>
+      log.info(s"being captured by Prison[$prison]")
+      prison ! CheckIn(name)
+
+    case Imprisoned(message) =>
+      log.info(s"got imprisoned: [$message]")
+
+    case KickedOut(reason) =>
+      log.info(s"got kicked out: [$reason]")
+      context.system.stop(self)
+
+    case Question(accomplice) =>
+      log.debug(s"questioning starts against Accomplice[$accomplice]")
+      sender() ! PrisonerResponse(if (rnd.nextBoolean()) PrisonerDecision.Accuse else PrisonerDecision.Refuse)
+
+    case QuestionResult(accomplice, decision) =>
+      log.info(s"questioning ends against Accomplice[$accomplice] and Decision[$decision]")
+
+    case unknown =>
+      log.debug(s"received unknown message [${unknown.getClass.getSimpleName}]")
   }
 }
